5691057105b907f316e6aa97e0d446a7a5047062
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-12-11 16:37:11 +0800

    Add files via upload

---
+++ Class03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Class03 {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sum=(1+n)*n/2;
+        System.out.println(sum);
+    }
+}
